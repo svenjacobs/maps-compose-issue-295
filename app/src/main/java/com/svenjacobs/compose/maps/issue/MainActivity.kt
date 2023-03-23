a17e33a56c3b291ba45481a1e4813a788d3cc54a
Sven Jacobs <github@svenjacobs.com>
2023-03-23 09:01:08 +0100

    Initial commit :)

---
+++ app/src/main/java/com/svenjacobs/compose/maps/issue/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.svenjacobs.compose.maps.issue
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.google.maps.android.compose.GoogleMap
+import com.svenjacobs.compose.maps.issue.ui.theme.MyApplicationTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MyApplicationTheme {
+                Column {
+                    GoogleMap(
+                        modifier = Modifier.fillMaxWidth().weight(1f),
+                    )
+
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(min = 200.dp)
+                            .background(Color.Yellow)
+                    ) {
+                        Text("Hello world")
+                    }
+                }
+            }
+        }
+    }
+}
