dab28d0fa5a3bfb70d8a89207daf67c1099ac8c7
Sven Jacobs <github@svenjacobs.com>
2023-04-06 10:01:54 +0200

    Update dependencies (BOM 2023.04.00)

--- app/src/main/java/com/svenjacobs/compose/maps/issue/MainActivity.kt
+++ app/src/main/java/com/svenjacobs/compose/maps/issue/MainActivity.kt
@@ -6,16 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.maps.android.compose.GoogleMap
 import com.svenjacobs.compose.maps.issue.ui.theme.MyApplicationTheme
